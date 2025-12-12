e7e8c177b4ccf2ec30f81577024b41d8bf015f20
mariya-4002 <mariyakb890@gmail.com>
2025-12-12 20:11:31 +0530

    Updated control.py with @api_view and bug fixes

--- todolist/models.py
+++ todolist/models.py
@@ -4,5 +4,59 @@ class Todo(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
 
-    def __str__(self):
+    def _str_(self):
         return self.title
+
+    # -------------------------
+    # CREATE – Save data
+    # -------------------------
+    @staticmethod
+    def create_todo(data):
+        todo = Todo.objects.create(
+            title=data.title,
+            description=data.description
+        )
+        return todo
+
+    # -------------------------
+    # UPDATE – Update existing
+    # -------------------------
+    @staticmethod
+    def update_todo(todo_id, data):
+        try:
+            todo = Todo.objects.get(id=todo_id)
+            todo.title = data.title
+            todo.description = data.description
+            todo.save()
+            return todo
+        except Todo.DoesNotExist:
+            return None
+
+    # -------------------------
+    # GET SINGLE – Fetch one
+    # -------------------------
+    @staticmethod
+    def get_todo(todo_id):
+        try:
+            return Todo.objects.get(id=todo_id)
+        except Todo.DoesNotExist:
+            return None
+
+    # -------------------------
+    # GET ALL – Fetch all todos
+    # -------------------------
+    @staticmethod
+    def get_all_todos():
+        return Todo.objects.all()
+
+    # -------------------------
+    # DELETE – Remove a todo
+    # -------------------------
+    @staticmethod
+    def delete_todo(todo_id):
+        try:
+            todo = Todo.objects.get(id=todo_id)
+            todo.delete()
+            return True
+        except Todo.DoesNotExist:
+            return False
